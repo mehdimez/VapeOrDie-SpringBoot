a67d484182732b97e46bea8096aad9999b83e95b
mehdimez <mahdimez@gmail.com>
2019-11-20 19:28:28 +0100

    nothing changed

--- src/main/java/iset/miniprj/vapeOrDie/entities/Category.java
+++ src/main/java/iset/miniprj/vapeOrDie/entities/Category.java
@@ -25,7 +25,6 @@ public class Category {
     @Column(name = "id_category")
     private long idCategory;
     private String name;
-
     @NotBlank(message = "Description is required")
     private String description;
 
